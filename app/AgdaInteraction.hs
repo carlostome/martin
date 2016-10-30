4e4e71e2fc777414034a1986d323e0ca4530deb7
Fabian Thorand <f.thorand@gmail.com>
2016-10-30 23:52:50 +0100

    Documentation and cleanup

--- app/AgdaInteraction.hs
+++ app/AgdaInteraction.hs
@@ -19,35 +19,24 @@ import           Agda.TypeChecking.Monad
 import           Agda.Utils.FileName
 import           Agda.Utils.Pretty
 
-import qualified MakeCaseModified                           as MC
-
-import           Control.DeepSeq
 import           Control.Lens
 import           Control.Monad.Except
-import           Control.Monad.IO.Class
 import           Control.Monad.Reader
 import           Control.Monad.State.Strict
 import           Control.Monad.Writer
-import qualified Data.List                                  as List
-import qualified Data.Set                                   as Set
-import           Debug.Trace
+import           Data.Maybe
 import           System.Console.Haskeline
 import qualified System.Console.Haskeline                   as HaskEx
-import           System.FilePath                            ((</>))
 import qualified Text.ParserCombinators.ReadP               as ReadP
 import qualified Text.ParserCombinators.ReadPrec            as ReadPrec
 import           Text.Printf
 import           Text.Read                                  (readPrec)
-import           Data.Generics.Geniplate
-import           Data.List                                  ((\\))
-import           Data.Maybe
-import           Data.Monoid
 
-import           SearchTree
 import qualified AgdaUtil                                   as AU
+import qualified MakeCaseModified                           as MC
+import qualified ProofSearch                                as Ps
 import qualified Strategy                                   as S
-import qualified ProofSearch                                   as Ps
-import qualified Translation                                   as T
+import qualified Translation                                as T
 
 -- | An exercise is just an Agda file, represented by the declarations inside it.
 -- INVARIANT: this state needs to be kept in sync with the TCM state.
@@ -58,6 +47,7 @@ data ExerciseState = ExerciseState
   { _exerciseProgram  :: S.StatefulProgram
   -- ^ the current state of the program
   , _exerciseStrategy :: S.ExerciseStrategy
+  -- ^ the strategy for solving the current program
   , _exerciseUndo     :: [(S.StatefulProgram, S.ExerciseStrategy)]
   -- ^ a history of program states
   }
@@ -67,16 +57,20 @@ makeLenses ''ExerciseState
 -- | Environment for exercise computation.
 data ExerciseEnv = ExerciseEnv
   { _exerciseFile    :: AbsolutePath
+  -- ^ the exercise file we have loaded
   , _exerciseTCState :: TCState
+  -- ^ the initial state of the type checker
   , _exerciseTCEnv   :: TCEnv
+  -- ^ the default environment for the type checker
   , _exerciseSession :: S.Session
+  -- ^ the Agda session for generating strategies
   }
 
 makeLenses ''ExerciseEnv
 
+-- | A prettified type checker error.
 newtype PrettyTCErr = PrettyTCErr { getPrettyTCErr :: String }
   deriving (Show)
-
 instance Exception PrettyTCErr
 
 -- | I'm sorry for another Monad stack, but somewhere we need to keep track of
@@ -136,23 +130,26 @@ runInteractiveSession verbosity agdaFile = do
             exerciseLoop
       void $ runStateT (runReaderT (runInputT defaultSettings go) exEnv) exState
 
+-- | The commands a user can perform at the top level interaction loop.
 data TopCommand
-  = CmdTopUndo
-  | CmdTopSelect Int
-  | CmdTopExit
-  | CmdTopHelp
-  | CmdTopPrint
+  = CmdTopUndo       -- ^ undo the last split or refine action
+  | CmdTopSelect Int -- ^ focus on a hole
+  | CmdTopExit       -- ^ exit the program
+  | CmdTopHelp       -- ^ print help message
+  | CmdTopPrint      -- ^ print the program
   deriving (Eq, Ord, Show, Read)
 
+-- | The commands a user can perform while being focused on a hole.
 data HoleCommand
-  = CmdHoleType
-  | CmdHoleContext
-  | CmdRefine String
-  | CmdSplit String
-  | CmdHoleLeave
-  | CmdHoleHint
+  = CmdHoleType      -- ^ print the hole type
+  | CmdHoleContext   -- ^ print the hole context
+  | CmdRefine String -- ^ refine the hole with the given expression
+  | CmdSplit String  -- ^ split on the given variable
+  | CmdHoleLeave     -- ^ leave the hole and return to the top level
+  | CmdHoleHint      -- ^ print a hint for the hole
   deriving (Eq, Ord, Show, Read)
 
+-- | Parser for TopCommands.
 readPTopCommand :: ReadP.ReadP TopCommand
 readPTopCommand = msum
   [ CmdTopUndo <$ ReadP.char 'u'
@@ -162,6 +159,7 @@ readPTopCommand = msum
   , CmdTopSelect <$> (ReadP.char 's' *> ReadP.skipSpaces *> ReadPrec.readPrec_to_P readPrec 0)
   ]
 
+-- | Parser for HoleCommands.
 readPHoleCommand :: ReadP.ReadP HoleCommand
 readPHoleCommand = msum
   [ CmdHoleType <$ ReadP.char 't'
@@ -172,14 +170,17 @@ readPHoleCommand = msum
   , CmdSplit <$> (ReadP.char 's' *> ReadP.skipSpaces *> ReadP.many1 ReadP.get)
   ]
 
+-- | Runs a parser.
 runReadP :: ReadP.ReadP a -> String -> Maybe a
 runReadP p s =
   case [ x | (x,"") <- ReadPrec.readPrec_to_S (ReadPrec.lift p) 0 s ] of
     [x] -> Just x
     _   -> Nothing
 
+-- | The top level interaction loop.
 exerciseLoop :: ExerciseM ()
 exerciseLoop = do
+  -- check if there are still holes to be filled
   tcs <- use $ exerciseProgram . S.programTCState
   (ips, _) <- runTCMEx tcs getInteractionPoints
   if null ips
@@ -226,12 +227,15 @@ exerciseLoop = do
         prettyProgram >>= outputStrLn
         exerciseLoop
 
+-- | Catches prettified TCM errors in the InputT monad, and runs the continuation
+-- from the error handler.
 catchTCMErr :: MonadException m => InputT m a -> InputT m a -> InputT m a
 catchTCMErr cont act = HaskEx.catch act $ \e -> do
   outputStrLn "Agda returned an error:\n"
   outputStrLn $ getPrettyTCErr e
   cont
 
+-- | Interactive loop when focused on a hole.
 holeLoop :: InteractionId -> ExerciseM ()
 holeLoop ii = do
   minput <- getInputLine $ show ii ++ "% "
@@ -267,9 +271,11 @@ holeLoop ii = do
           outputStrLn $ "Split variable '" ++ var ++ "'"
       holeLoop ii
 
+-- | Retrieves the strategy for the given hole from the state.
 getStrategyFor :: InteractionId -> ExerciseM (Maybe S.ClauseStrategy)
 getStrategyFor ii = getFirst <$> use (exerciseStrategy . ix (interactionId ii) . to First)
 
+-- | Executes a split action.
 splitUser :: InteractionId -> String -> ExerciseM ()
 splitUser ii var = do
   prog <- use exerciseProgram
@@ -277,7 +283,7 @@ splitUser ii var = do
   ((_, newClauses), _) <- runTCMEx (view S.programTCState prog) $ MC.makeCase ii noRange var
   let newDecls = AU.replaceClauses ii newClauses (view S.programDecls prog)
   --
-  updateProgramAndCheck newDecls
+  checkProgramAndUpdate newDecls
   -- check strategy
   st <- getStrategyFor ii
   let notWithStrat msg = outputStrLn msg >> regenerateStrategy
@@ -289,6 +295,7 @@ splitUser ii var = do
     Just (S.RefineStrategy _) -> notWithStrat "I chose to refine here."
     Nothing -> notWithStrat "I didn't know what to do here."
 
+-- | Executes a refinement action.
 refineUser :: InteractionId -> String -> ExerciseM ()
 refineUser ii def = do
   prog <- use exerciseProgram
@@ -298,7 +305,7 @@ refineUser ii def = do
     -- try to refine the hole with the user expression
     B.refine ii Nothing given
   let newDecls = AU.replaceHole ii expr (view S.programDecls prog)
-  updateProgramAndCheck newDecls
+  checkProgramAndUpdate newDecls
   -- check strategy
   st <- getStrategyFor ii
   -- TODO: do not regenerate strategy if the user actually did what we expected
@@ -306,8 +313,11 @@ refineUser ii def = do
     --Just (S.RefineStrategy prf) -> _
     _ -> regenerateStrategy
 
-updateProgramAndCheck :: [A.Declaration] -> ExerciseM ()
-updateProgramAndCheck newDecls = do
+-- | Type checks a program and updates the current program state
+-- with the new program and TCState if successful.
+-- The previous state is recorded in the undo-history.
+checkProgramAndUpdate :: [A.Declaration] -> ExerciseM ()
+checkProgramAndUpdate newDecls = do
   tcs <- view exerciseTCState
   (newDecls', progState) <- runTCMEx tcs $ do
     -- rebuild interaction points (normally only created when going from concrete -> abstract)
@@ -319,20 +329,23 @@ updateProgramAndCheck newDecls = do
   checkpoint
   exerciseProgram .= S.StatefulProgram newDecls' progState
 
+-- | Regenerates the strategy for the current program state.
 regenerateStrategy :: ExerciseM ()
 regenerateStrategy = do
   decls <- use $ exerciseProgram . S.programDecls
   strat <- view exerciseSession >>= \s -> liftIO $ S.buildStrategy s decls
   exerciseStrategy .= fromMaybe [] strat
 
+-- | @concatReplace i rs xs@ replaces the i-th element of @xs@ with @rs@.
 concatReplace :: Int -> [a] -> [a] -> [a]
-concatReplace n repl []
+concatReplace n _ []
   | n < 0 = []
 concatReplace n repl (x:xs)
   | n == 0 = repl ++ xs
   | n > 0 = x : concatReplace (n - 1) repl xs
 concatReplace _ _ _ = error "concatReplace: invalid arguments"
 
+-- | Reverts the program and strategy state to the previous undo-checkpoint.
 undo :: ExerciseM Bool
 undo = use exerciseUndo >>= \case
   [] -> return False
@@ -358,186 +371,17 @@ prettyProgram = do
     let decls = AU.setMetaNumbersToInteraction (view S.programDecls prog)
     render . vcat <$> mapM prettyA decls
 
+-- | Runs a TCM computation inside the exercise monad.
+-- All TCErr exceptions are converted to PrettyTCErr exceptions before
+-- being rethrown outside of the TCM monad.
 runTCMEx :: TCState -> TCM a -> ExerciseM (a, TCState)
 runTCMEx tcs tcm = do
   env <- view exerciseTCEnv
   liftIO $ runTCM env tcs $ tcm `catchError` (prettyError >=> HaskEx.throwIO . PrettyTCErr)
 
-    -- -- run user session
-    -- st <- execStateT (runReaderT exerciseSession exEnv) exState
-    -- -- if all went well, st contains the solved exercise
-    -- -- TODO: pretty print and send to teacher ;)
-    -- return ()
-  -- -- print errors
-{-
+-- * Orphan instances
 
-
--- | Type checks a new state of the program and update, if successful.
--- May fail with an exception if any step goes wrong. Restoring state
--- should be handled by caller.
-performUserAction :: InteractionId -> UserAction -> ExerciseM ()
-performUserAction hole action = do
-  -- apply action to generate new program
-  newprog <- case action of
-    UserRefine estr -> do
-      -- parse the user input in the given context
-      given <- tcmToEx $ B.parseExprIn hole noRange estr
-      -- try to refine the hole with the user expression
-      expr <- tcmToEx $ B.give hole Nothing given
-      -- replace hole in AST
-      replaceHole hole expr <$> gets exerciseDecls
-    UserSplit var -> do
-      -- invoke case splitting functionality
-      (ctx, newClauses) <- tcmToEx $ MC.makeCase hole noRange var
-      -- ctx seems only to be relevant when splitting in extended lambdas, not something we do
-      replaceClauses hole newClauses <$> gets exerciseDecls
-  -- type check
-  resetTCState
-  -- rebuild interaction points (normally only created when going from concrete -> abstract)
-  newprog' <- rebuildInteractionPoints newprog
-  -- check updated AST (might not succeed if the termination checker intervenes)
-  tcmToEx $ do
-    checkDecls newprog'
-    unfreezeMetas
-  modify $ \s -> s { exerciseDecls = newprog' }
-
-undo :: ExerciseM Bool
-undo = do
-  hist <- gets exerciseUndo
-  case hist of
-    (prog,st):rest -> do
-      restoreTCState st
-      tcmToEx $ getInteractionIdsAndMetas >>= liftIO . print
-      modify $ \s -> s { exerciseUndo = rest, exerciseDecls = prog }
-      return True
-    _ -> return False
-
--- | Takes a proof and converts it to an abstract expression in the context of the given hole.
-proofToAbstractExpr :: InteractionId -> Proof -> TCM A.Expr
-proofToAbstractExpr ii proof = B.parseExprIn ii noRange (proofStr proof) where
-  proofStr (Proof name args _)
-    | List.null args = name
-    | otherwise = "(" ++ List.unwords (name : map proofStr args) ++ ")"
-
--- | Replaces a hole identified by its interaction id with a new expression.
-replaceHole :: A.ExprLike e => InteractionId -> A.Expr -> e -> e
-replaceHole ii repl = A.mapExpr $ \e -> case e of
-                                  A.QuestionMark _ iiq
-                                    | iiq == ii -> repl
-                                  other -> other
--- | Replaces all question marks with fresh interaction points and registers them with the type checker.
--- This step is necessary after resetting the type checker state.
-rebuildInteractionPoints :: A.ExprLike e => e -> ExerciseM e
-rebuildInteractionPoints = tcmToEx . A.traverseExpr go where
-  go (A.QuestionMark m _) = A.QuestionMark m <$> registerInteractionPoint noRange Nothing
-  go other = return other
-
--- | Reverts to a fresh Agda TCM state, forgetting all user definitions and retaining only the primitives
-resetTCState :: ExerciseM ()
-resetTCState = asks exerciseInitialAgdaState >>= restoreTCState
-
--- | Lift a TCM computation in the exercise monad.
--- Can't use 'MonadTCM' because that requires reader and state over
--- the TCM env and state and not our state.
-tcmToEx :: TCM a -> ExerciseM a
-tcmToEx = lift . lift
-
--- | Try a computation, executing either the success handler with the result
--- or the error handler with the caught exception.
-tryIt :: MonadError e m => m a -> (a -> m b) -> (e -> m b) -> m b
-tryIt act success failure = do
-  r <- fmap Right act `catchError` \e -> return $ Left e
-  either failure success r
-
--- | Returns the current TCM state.
-saveTCState :: ExerciseM TCState
-saveTCState = tcmToEx get
-
--- | Restores the TCM state.
-restoreTCState :: TCState -> ExerciseM ()
-restoreTCState = tcmToEx . put
-
--- | Displays the current state of the program to the user.
--- It also shows the numbers (InteractionId) of each hole. Based on that, the user can then
--- choose to perform an action on a given hole.
-showProgramToUser :: ExerciseM ()
-showProgramToUser = do
-    prog <- setMetaNumbersToInteraction <$> gets exerciseDecls
-    doc <- tcmToEx $ vcat <$> mapM prettyA prog  -- printDecls prog
-    liftIO $ putStrLn $ render doc
-
-refineWithProof :: InteractionId -> Proof -> ExerciseM ()
-refineWithProof hole proof = return ()
-
--- * Interfacing with Agda
-
-
--- | This initializes the TCM state just enough to get everything started.
--- For now, it uses the default options and loads Agda's Level primitives.
-initAgda :: Int -> TCM TCState
-initAgda verbosity = do
-  -- initialize interactive state
-  resetAllState
-  setCommandLineOptions defaultOptions
-    { optPragmaOptions = (optPragmaOptions defaultOptions)
-      { optVerbose = Trie.singleton [] verbosity }
-    }
-
-  -- ==================== BEGIN CODE FROM AGDA SOURCE
-  libdir <- liftIO defaultLibDir
-  -- To allow posulating the built-ins, check the primitive module
-  -- in unsafe mode
-  _ <- bracket_ (gets Lens.getSafeMode) Lens.putSafeMode $ do
-    Lens.putSafeMode False
-    -- Turn off import-chasing messages.
-    -- We have to modify the persistent verbosity setting, since
-    -- getInterface resets the current verbosity settings to the persistent ones.
-    bracket_ (gets Lens.getPersistentVerbosity) Lens.putPersistentVerbosity $ do
-      Lens.modifyPersistentVerbosity (Trie.delete [])  -- set root verbosity to 0
-      -- We don't want to generate highlighting information for Agda.Primitive.
-      withHighlightingLevel None $ do
-        primitiveModule <- moduleName $ mkAbsolute $
-            libdir </> "prim" </> "Agda" </> "Primitive.agda"
-        getInterface_ primitiveModule
-  -- ==================== END CODE FROM AGDA SOURCE
-  -- return with state right after initialization, to be able to speed up things when we need to reset
-  get
-
-
--- * Some functions solely used for testing stuff
-
-itest2 :: FilePath -> IO ()
-itest2 agdaFile = do
-  -- load agda file
-  path <- absolute agdaFile
-  (pragmas, concreteDecls) <- parseFile' moduleParser path
-  -- run TCM computation (inside Agda's type checking monad)
-  runTCMPrettyErrors $ do
-    -- load primitives like Level etc.
-    initialState <- initAgda 0
-    -- convert to abstract syntax
-    abstractDecls <- toAbstract concreteDecls
-    checkDecls abstractDecls
-    -- print all interaction metas (i.e. holes in the program)
-    getInteractionIdsAndMetas >>= liftIO . print
-
-    -- TEST CODE for map function
-    [(inil,mnil), (icons,mcons)] <- getInteractionIdsAndMetas
-    forM_ itWorks $ \prf -> do
-      liftIO $ print prf
-      given <- proofToAbstractExpr icons prf
-      pp <- prettyA given
-      liftIO $ print $ render pp
-    -- END TEST CODE
-
-    -- try restoring the state
-    put initialState
-    getInteractionIdsAndMetas >>= liftIO . print -- prints [], so restoring actually works
-
-    return ()
-
--}
-
+-- these orphan instances are required to make InputT work on top of our own monad transformer stack
 
 instance MonadState s m => MonadState s (InputT m) where
   get = lift get
